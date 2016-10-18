596e327a9c346aeaed44a1d7b9a29fd47d207ad4
matrix.static <matrix.static@gmail.com>
2016-10-18 17:34:58 +0800

    add readTree for JsonHelper

--- src/edex-framework/src/main/java/com/edexsoft/framework/utility/JsonHelper.java
+++ src/edex-framework/src/main/java/com/edexsoft/framework/utility/JsonHelper.java
@@ -1,6 +1,10 @@
 package com.edexsoft.framework.utility;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 import com.edexsoft.framework.EdexException;
 
@@ -8,46 +12,58 @@ import com.edexsoft.framework.EdexException;
 public class JsonHelper {
 	private static final ObjectMapper objMapper;
 
-    static {
-        objMapper = new ObjectMapper();
-    }
+	static {
+		objMapper = new ObjectMapper();
+	}
 
-    /**
-     * Convert object <code>o</code> to json format
-     *
-     * @param o
-     * @return
-     */
-    public static String toJson(Object o) {
-        try {
-            return objMapper.writeValueAsString(o);
-        } catch (Exception e) {
-            throw new EdexException(e);
-        }
-    }
+	/**
+	 * Convert object <code>o</code> to json format
+	 *
+	 * @param o
+	 * @return
+	 */
+	public static String toJson(Object o) {
+		try {
+			return objMapper.writeValueAsString(o);
+		} catch (Exception e) {
+			throw new EdexException(e);
+		}
+	}
 
-    /**
-     * Convert json value <code>value</code> to java object has class type
-     * <code>type</code>
-     *
-     * @param value
-     * @param type
-     * @return
-     */
-    public static <T> T fromJson(String value, Class<T> type) {
-        try {
-            T ins = objMapper.readValue(value, type);
-            if (ins == null) {
-                try {
-                    return type.newInstance();
-                } catch (Exception e) {
-                    throw new EdexException(e);
-                }
-            }
+	/**
+	 * Convert json value <code>value</code> to java object has class type
+	 * <code>type</code>
+	 *
+	 * @param value
+	 * @param type
+	 * @return
+	 */
+	public static <T> T fromJson(String value, Class<T> type) {
+		try {
+			T ins = objMapper.readValue(value, type);
+			if (ins == null) {
+				try {
+					return type.newInstance();
+				} catch (Exception e) {
+					throw new EdexException(e);
+				}
+			}
 
-            return ins;
-        } catch (Exception e) {
-            throw new EdexException(e);
-        }
-    }
+			return ins;
+		} catch (Exception e) {
+			throw new EdexException(e);
+		}
+	}
+
+	public static JsonNode readTree(String jsonEntity) {
+		JsonNode oJsonNode = null;
+		try {
+			oJsonNode = objMapper.readTree(jsonEntity);
+		} catch (JsonProcessingException e) {
+//			logger.error(e.toString());
+		} catch (IOException e) {
+//			logger.error(e.toString());
+		}
+		return oJsonNode;
+	}
 }
